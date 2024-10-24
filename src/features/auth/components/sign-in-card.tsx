6aa14306ac5089b3f4e3c34bffb85b14813cb11d
Manish <tamangmanish3456@gmail.com>
2024-10-24 13:39:43 +0545

    add sign-in ui

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,73 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export function SignInCard() {
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome back👋</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            type="email"
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter email"
+            disabled={false}
+            required
+          />
+          <Input
+            type="password"
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={256}
+            required
+          />
+          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant={"secondary"}
+          size={"lg"}
+          className="w-full"
+          disabled={false}
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <div className="flex gap-2">
+          <DottedSeparator />
+          Or
+          <DottedSeparator />
+        </div>
+        <Button
+          variant={"secondary"}
+          size={"lg"}
+          className="w-full"
+          disabled={false}
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
