8f3016d8884bdbd6827d27ef08dfc00712f38d51
Manish <tamangmanish3456@gmail.com>
2024-10-24 13:40:31 +0545

    add auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex items-center justify-between">
+          <Image src={"/logo.svg"} height={152} width={56} alt="logo" />
+
+          <Button variant={"secondary"}>Sign up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
