2084e66243a67d6ba723762c0be22d02268ca2a6
Manish <tamangmanish3456@gmail.com>
2024-10-25 14:13:53 +0545

    add useLogin

---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+
+import { loginSchema } from "../schemas";
+
+const app = new Hono()
+  .post(
+    "/login", 
+    zValidator("json", loginSchema), c => {
+    return c.json({
+        success: 200
+    });
+});
+
+export default app;
